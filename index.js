b936f485f7e2bedb17d64a157b4384eea6f714da
hyunjinnnnnp <56399099+hyunjinnnnnp@users.noreply.github.com>
2020-11-28 18:31:25 +0900

    mouse location, fade, blur

--- index.js
+++ index.js
@@ -1,9 +1,44 @@
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 
-function init() {}
+const mouse = {
+  x: undefined,
+  y: undefined,
+};
 
-window.addEventListener("resize", resizeCanvas);
+function fadeOut() {
+  let opacity = 0.1 + Math.random() * 0.1;
+  ctx.fillStyle = `rgb(39, 37, 37,${opacity})`;
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  setTimeout(fadeOut, 100);
+}
+
+function draw() {
+  const random = Math.random() * 20;
+
+  const r = 20;
+
+  ctx.beginPath();
+  ctx.filter = "blur(10px)";
+  ctx.fillStyle = "rgba(255,255,255,1)";
+  for (let i = 0; i < 10; i++) {
+    ctx.arc(mouse.x + random, mouse.y + random, r + random, 0, 360);
+  }
+  ctx.fill();
+}
+
+function handleMouse(e) {
+  mouse.x = e.pageX - this.offsetLeft;
+  mouse.y = e.pageY - this.offsetTop;
+
+  draw();
+}
+
+function init() {
+  canvas.addEventListener("mousemove", handleMouse);
+  window.addEventListener("resize", resizeCanvas);
+  fadeOut();
+}
 
 function resizeCanvas() {
   canvas.width = window.innerWidth;
